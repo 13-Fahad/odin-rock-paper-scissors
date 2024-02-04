47c2a179d55fc81b9aa2f9ec707691e42dca8d1a
Fahad Mostafa <13.fahad.mostafa@gmail.com>
2024-02-04 16:19:39 +0000

    Add getComputerChoice function
    This function randomly returns the strings "Rock" "Paper" or "Scissors"

--- javascript.js
+++ javascript.js
@@ -1,2 +1,12 @@
 "use strict";
 
+function getComputerChoice() {
+    let randomInt = Math.floor(Math.random() * 3)
+    if (randomInt === 0) {
+        return "Rock";
+    } else if (randomInt === 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
