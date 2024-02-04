5352dc193cb77832d7d5ac1e9940cd76878a9421
Fahad Mostafa <13.fahad.mostafa@gmail.com>
2024-02-04 17:54:01 +0000

    Add playRound() function

--- javascript.js
+++ javascript.js
@@ -2,11 +2,36 @@
 
 function getComputerChoice() {
     let randomInt = Math.floor(Math.random() * 3)
-    if (randomInt === 0) {
-        return "Rock";
-    } else if (randomInt === 1) {
-        return "Paper";
+        if (randomInt === 0) {
+            return "Rock";
+        } else if (randomInt === 1) {
+            return "Paper";
+        } else {
+            return "Scissors";
+        }
+}
+
+const computerSelection = getComputerChoice();
+
+const playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+
+function playRound(playerSelection, computerSelection) {
+
+    if (computerSelection === playerSelection) {
+        return console.log("The computer picked the same as you.");
+    } else if (computerSelection === "Rock" && playerSelection === "scissors") {
+        return console.log("The computer picked Rock. You lost.")
+    } else if (computerSelection === "Rock" && playerSelection === "paper") {
+        return console.log("The computer picked Rock. You won.")
+     } else if (computerSelection === "Paper" && playerSelection === "rock") {
+        return console.log("The computer picked Paper. You lost.")
+    } else if (computerSelection === "Paper" && playerSelection === "scissors") {
+        return console.log("The computer picked Paper. You won.")
+    } else if (computerSelection === "Scissors" && playerSelection === "paper") {
+        return console.log("The computer picked Scissors. You lost.")
+    } else if (computerSelection === "Scissors" && playerSelection === "rock") {
+        return console.log("The computer picked Scissors. You won.")
     } else {
-        return "Scissors";
+        return console.log("Please enter 'Rock' 'Paper' or 'Scissors'")
     }
 }
